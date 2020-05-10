5ea8b1ee5b80e6fa55738dade082eeec56bb5cd2
avery rouser <arouser2112@gmai.com>
2020-05-09 23:00:22 -0500

    deployable app

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <h2 className="navbar-item" href="/">
+        Employee Directory
+      </h2>
+    </nav>
+  );
+}
+
+export default Navbar;
